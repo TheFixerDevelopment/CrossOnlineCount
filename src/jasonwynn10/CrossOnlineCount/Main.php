6960dd9b2c533ce6f4de06371ac8e8812a6786f8
jasonwynn10 <jasonwynn10@gmail.com>
2017-08-04 21:05:21 -0400

    Fixed issues being caused by regex close #1 close #2

--- src/jasonwynn10/CrossOnlineCount/Main.php
+++ src/jasonwynn10/CrossOnlineCount/Main.php
@@ -97,9 +97,9 @@ class Main extends PluginBase implements Listener {
 	 * @return bool
 	 */
 	public function is_valid_domain_name(string $domain_name) {
-		return (preg_match("/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain_name) //valid chars check
-		        and preg_match("/^.{1,253}$/", $domain_name) //overall length check
-		        and preg_match("/^[^\.]{1,63}(\.[^\.]{1,63})*$/", $domain_name)); //length of each label
+		return (preg_match("/([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*:(\d{1,5})/i", $domain_name) //valid chars check
+		        and preg_match("/.{1,253}/", $domain_name) //overall length check
+		        and preg_match("/[^\.]{1,63}(\.[^\.]{1,63})*/", $domain_name)); //length of each label
 	}
 	/**
 	 * @api
