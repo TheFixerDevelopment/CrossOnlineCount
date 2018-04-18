240add3e4db30ddc2b7ad183a314bd9693da99bf
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-04-18 14:07:56 +0100

    Update UpdateTask.php

--- src/jasonwynn10/CrossOnlineCount/UpdateTask.php
+++ src/jasonwynn10/CrossOnlineCount/UpdateTask.php
@@ -4,7 +4,7 @@ namespace jasonwynn10\CrossOnlineCount;
 use pocketmine\scheduler\PluginTask;
 
 class UpdateTask extends PluginTask {
-	public function onRun($currentTick) {
+	public function onRun(int $currentTick) {
 		$this->getOwner()->update();
 	}
 }
